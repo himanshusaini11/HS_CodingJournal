b7de865403ebed041949d84676ca2be377531c1e
Himanshu Saini <sainih5@outlook.com>
2024-10-11 18:46:37 -0400

    Updated

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def home():
     return "Hello, this is a test app."
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
