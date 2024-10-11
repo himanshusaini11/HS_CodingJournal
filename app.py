60ed85c4a754578a811474e732fe5e463eb785c8
Himanshu Saini <sainih5@outlook.com>
2024-10-11 18:13:37 -0400

    Updated

--- app.py
+++ app.py
@@ -225,5 +225,5 @@ def k_fold_cross_validation(X, y, model, k=5):
 
 # Run the app.
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run_server(debug=False, host='0.0.0.0', port=port)
+    #port = int(os.environ.get('PORT', 8000))
+    app.run_server(debug=False)#, host='0.0.0.0', port=port)
